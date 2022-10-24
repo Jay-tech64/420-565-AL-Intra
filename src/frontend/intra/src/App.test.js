10b13341ba3bb70bd1f7af01617bee89ed25e6c1
Jérémy Mailhot <jaytech64@pm.me>
2022-10-24 12:41:53 -0400

    Écriture des tests pour le frontend

--- src/frontend/intra/src/App.test.js
+++ src/frontend/intra/src/App.test.js
@@ -1,8 +1,26 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("App", () => {
+  it("should render inputs", () => {
+    render(<App />);
+
+    const addInputOne = screen.getByPlaceholderText(/One/i);
+    const addInputTwo = screen.getByPlaceholderText(/Two/i);
+
+    expect(addInputOne).toBeInTheDocument();
+    expect(addInputTwo).toBeInTheDocument();
+  });
+
+  it("should add text to input", () => {
+    render(<App />);
+
+    const addInputOne = screen.getByPlaceholderText(/One/i);
+    const addInputTwo = screen.getByPlaceholderText(/Two/i);
+    fireEvent.change(addInputOne, { target: { value: "12" } });
+    fireEvent.change(addInputTwo, { target: { value: "4" } });
+
+    expect(addInputOne.value).toBe("12");
+    expect(addInputTwo.value).toBe("4");
+  });
 });
